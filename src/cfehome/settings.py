a04fd321f33eabaa677bbeacb2ce7f310deddd34
vaibhavthakur9580 <thakurvaibhav9580@gmail.com>
2025-06-04 16:56:32 +0530

    database changes

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,11 @@ print("base_dir",BASE_DIR)
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i0ylusj-!w-+^l#^ng8$p!3o(4s@=m!*87*obr(jeq*_+$8%#^"
+SECRET_KEY = config("DJANGO_SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+DEBUG = config("DJANGO_DEBUG")
 
 print("DEBUG", DEBUG, type(DEBUG))
 
@@ -90,6 +92,20 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+}
+
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    "default": dj_database_url.config(
+        default=DATABASE_URL,
+        # setting up the connection health checks 
+        conn_max_age=CONN_MAX_AGE,
+        conn_health_checks=True
+    )
 }
 
 
