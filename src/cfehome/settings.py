e8529076e22870c68accdd5292431b28908d0181
vaibhavthakur9580 <thakurvaibhav9580@gmail.com>
2025-06-05 13:52:43 +0530

    prepare static files

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -144,6 +144,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDORS_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# source for python  manage.py collectstatic 
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+
+# output for python  manage.py collectstatic 
+# local_cdn
+STATIC_ROOT = BASE_DIR / "local-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
